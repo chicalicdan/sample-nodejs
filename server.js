f1fb1c3b87428b0e2f4577c8d2aaad3dc57d6922
Chi Calicdan <chicalicdan@gmail.com>
2022-02-11 11:24:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello from middleware-hook!')
+    res.send('Test AWS CodePipeline')
 })
 
 app.listen(3000, () => {
-    console.log('Server is up for middleware-hook')
+    console.log('Test AWS CodePipeline')
 })
