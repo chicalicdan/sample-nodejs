8628c85da1ff047842c4594e5f01b08327db4083
Chi Calicdan <chicalicdan@gmail.com>
2022-02-11 11:48:15 +0800

    Test Codepipeline deploy

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Test AWS CodePipeline')
+    res.send('Test AWS CodePipeline deploy')
 })
 
 app.listen(3000, () => {
-    console.log('Test AWS CodePipeline')
+    console.log('Test AWS CodePipeline deploy')
 })
